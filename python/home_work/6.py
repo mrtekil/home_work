b67e4910adb7bbeb7fa75e49bdb8149c26c55ba1
mrbain <the-god@bk.ru>
2024-08-15 22:00:04 +0300

    new file

---
+++ python/home_work/6.py
@@ -0,0 +1,15 @@
+word = input("Введите слово из маленьких латинских букв: ")
+
+glasniye = 0
+soglasniye = 0
+
+for letter in word:
+    if letter in "aeiou":
+        glasniye += 1
+    elif letter in "bcdfghjklmnpqrstvwxyz":
+        soglasniye += 1
+else:
+    print(False)
+
+print("Количество гласных букв:", glasniye)
+print("Количество согласных букв:", soglasniye)
